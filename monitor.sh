bead49aaf11d5010d6a2c7145a185a5ddd8c303d
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-01-22 15:59:45 +0800

    first init

---
+++ monitor.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+pidfile="pid-bg.log"
+if [ -s $pidfile ];then
+    pids=`cat $pidfile`
+    echo "----kill pids: $pids-----"
+    kill $pids
+    rm -rf $pidfile
+    exit
+fi
+
+if [ $# -lt 1 ];then
+    echo "--need identifier!!---"
+    exit
+fi
+
+pids=""
+if [ $1 == '-ui' ];then
+    #upper identify
+    dirName=$2
+fi
+
+if [ -z $dirName ];then
+    nodeName="$HOSTNAME"
+    identify=$1
+    dirName=$nodeName-$identify
+fi
+
+echo "-----log dir:$dirName------"
+if [ -d $dirName ];then
+    rm -rf $dirName
+fi
+mkdir $dirName
+
+command -v dstat >/dev/null 2>&1 || yum install dstat
+command -v pidstat >/dev/null 2>&1 || yum install sysstat
+
+#disk
+iostat sda sdb sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm 1 -m > $dirName/disk.log &
+pids="$!"
+
+#net
+sar -n DEV 1 > $dirName/net.log 	&
+pids="$pids $!"
+
+#cpu
+sar -u 1 > $dirName/cpu.log	&
+pids="$pids $!"
+
+sar -P ALL 1 > $dirName/cpuPer.log 	&
+pids="$pids $!"
+
+#mem
+free -c 3600 -s 1 -h > $dirName/mem.log &
+pids="$pids $!"
+
+#pidstat
+pidstat -l -t -d -u -C "fio|tgtd|icfs|ceph-osd|radosgw" 1 > $dirName/pidstat.log &
+pids="$pids $!"
+
+#dstat
+dstat --nocolor > $dirName/dstat.log &
+pids="$pids $!"
+
+echo $pids > $pidfile
+echo "-----bg pids:$pids----------------"
---
+++ rcb.sh
@@ -0,0 +1,342 @@
+#!/bin/bash
+
+nodes=""
+nodesPwds="
+    127.0.0.1,IPS@jjfab2018
+    192.168.100.100,IPS@jjfab2018
+    192.168.100.101,IPS@jjfab2018
+    192.168.100.102,IPS@jjfab2018
+"
+
+#issue fileNames
+issues="
+    write
+    read
+    delete
+"
+
+monScript="./monitor.sh"
+nodeinfos=""
+nodeinfoFile='./nodeinfo.log'
+
+SSHPSCP="sshpass -p \$(gotNodePwd \$node) scp"
+SSHPSSH="sshpass -p \$(gotNodePwd \$node) ssh"
+
+function doInit() {
+    command -v sshpass >/dev/null 2>&1 || yum install sshpass	    #need epel
+    command -v fio >/dev/null 2>&1 || yum install fio
+
+    for np in $nodesPwds;do
+	n=${np%,*}
+	nodes="$nodes $n"
+    done
+
+    node=$n
+    #echo "--------$SSHPSCP"
+}
+
+function gotNodePwd(){
+    node=$1
+    [ -n $node ] || return
+    npMatched=""
+    for np in $nodesPwds;do
+	if [ $node == `echo $np | awk 'BEGIN {FS=","} {print $1}'` ];then
+	    #echo "match node:$node"
+	    npMatched=$np
+	    break
+	fi
+    done
+
+    if [ -n $npMatched ];then
+	pwd=`echo $npMatched | awk 'BEGIN {FS=","} {print $2}'`
+    else
+	return
+    fi
+    echo "$pwd"
+}
+
+
+function saveNodeinfo() {
+    node=$1
+    info=$2
+    echo "do saveNodeinfo for node:$node,info:$info"
+    for nodeinfo in $nodeinfos;do
+	if [ $node == `echo $nodeinfo | awk 'BEGIN {FS=","} {print $1}'` ];then
+	    #echo "match node:$node"
+	    infoMatched=$nodeinfo
+	    break
+	fi
+    done
+
+    if [ -n "$infoMatched" ];then
+	nodeinfos=`echo $nodeinfos | sed "s#$infoMatched##"`
+	info="$infoMatched,$info"
+    fi
+
+    nodeinfos="$nodeinfos $info"
+    #echo "updated nodeinfos:$nodeinfos---"
+
+    echo "$nodeinfos" > $nodeinfoFile
+}
+
+function gotNodeinfos() {
+    if [ -z "$nodeinfos" ];then
+	if ! [ -s "$nodeinfoFile" ];then
+	    exit
+	fi
+	nodeinfos=`cat $nodeinfoFile`
+	#rm -f $nodeinfoFile
+    fi
+    echo "$nodeinfos"
+}
+
+function gotWorkDir(){
+    node=$1
+    [ -n $node ] || return
+    infos=`gotNodeinfos`
+    for info in $infos ;do
+	if [ $node == `echo $info | awk 'BEGIN {FS=","} {print $1}'` ];then
+	    break
+	fi
+    done
+
+    workDir=`echo $info | awk 'BEGIN {FS=","} {print $2}'`
+    echo $workDir
+}
+
+function gotIdentify(){
+    node=$1
+    [ -n $node ] || return
+    for info in `gotNodeinfos`;do
+	if [ $node == `echo $info | awk 'BEGIN {FS=","} {print $1}'` ];then
+	    break
+	fi
+    done
+
+    identify=${info##*,}
+    echo $identify
+}
+
+function preMon(){
+    for node in $nodes;do
+	echo "do preMon for node:$node"
+	workDir=`sshpass -p $(gotNodePwd $node) ssh $node mktemp -d`
+	case $? in
+	    0)
+		;;
+	    5)
+		echo "--Invalid/incorrect password"
+		exit
+		;;
+	    6)
+		echo "--sshpass exits without confirming the new key"
+		exit
+		;;
+	    *)
+		echo "--sshpass error,exit"
+		exit
+	esac
+
+	#nName=`sshpass -p $(gotNodePwd $node) ssh $node hostname`
+	if [ -z $dryRun ];then
+	    sshpass -p $(gotNodePwd $node) scp $monScript root@$node:$workDir
+	    sshpass -p $(gotNodePwd $node) ssh $node "cd $workDir&&chmod +x ./$monScript"
+	    if [ $node != '127.0.0.1' ];then
+		sshpass -p $(gotNodePwd $node) ssh $node "echo 1 > /proc/sys/vm/drop_caches"
+	    fi
+	fi
+	info="$node,$workDir"
+	saveNodeinfo $node "$info"
+    done
+
+    echo
+}
+
+function startMon(){
+    idtSuffix=$1
+    [ -n $idtSuffix ] || idtSuffix="myRead"
+    for node in $nodes;do
+	echo "do startMon for node:$node"
+	nName=`sshpass -p $(gotNodePwd $node) ssh $node hostname`
+	identify="$nName-$idtSuffix"
+	workDir=`gotWorkDir $node`
+	if [ -z $dryRun ];then
+	    sshpass -p $(gotNodePwd $node) ssh $node "cd $workDir && ./$monScript -ui $identify"
+	fi
+	saveNodeinfo $node $identify
+    done
+    echo
+}
+
+function stopMonGetRet(){
+    for node in $nodes;do
+	echo "do stopMonGetRet for node:$node"
+	workDir=`gotWorkDir $node`
+	identify=`gotIdentify $node`
+	#echo "wkdir:$workDir"
+	#echo "idt:$identify"
+
+	if [ -z "$workDir" ];then
+	    echo '---warning--- workDir'
+	    exit
+	fi
+
+	if [ -z $dryRun ];then
+	    sshpass -p $(gotNodePwd $node) ssh $node "cd $workDir && ./$monScript"
+	    sshpass -p $(gotNodePwd $node) scp -r root@$node:$workDir/$identify ./$resDir
+	fi
+	echo
+    done
+    echo
+}
+
+function postMon() {
+    for node in $nodes;do
+	echo "do postMon for node:$node"
+	workDir=`gotWorkDir $node`
+	#echo $workDir
+
+	if [ -z "$workDir" ];then
+	    echo '---warning--- workDir'
+	    exit
+	fi
+
+	if [ -z $dryRun ];then
+	    sshpass -p $(gotNodePwd $node) ssh $node "rm -rf $workDir"
+	fi
+    done
+    echo
+}
+
+function doClean() {
+    for node in $nodes;do
+	echo "do doClean for node:$node"
+	workDir=`gotWorkDir $node`
+	identify=`gotIdentify $node`
+	echo "wkdir:$workDir"
+	echo "idt:$identify"
+
+	if [ -z "$workDir" ];then
+	    echo '---warning--- workDir'
+	    exit
+	fi
+
+	if [ -z $dryRun ];then
+	    sshpass -p $(gotNodePwd $node) ssh $node "cd $workDir && ./$monScript"
+	    if [ $? ];then
+		sshpass -p $(gotNodePwd $node) ssh $node "rm -rf $workDir"
+	    fi
+	fi
+	echo
+    done
+    echo
+}
+
+function docbIssues() {
+    issues="$@"
+    for issue in $issues ;do
+	if ! [ -s $issue ];then
+	    echo "issue file $issue not exist break"
+	    break
+	fi
+
+	echo -e "\033[0;1;31m--do cosbench for issue $issue--\033[0m"
+
+
+	# cbTest/10m-delete.xml
+	testType=${issue#*/}		#-->1m-delete.xml
+	testType=${testType%-*.xml}	#-->10m
+	echo "testType $testType"
+	if [ -z $dryRun ];then
+	    resDir="$testType-res-`date +%Y%m%d-%H%M%S`"
+	    if [ -d $resDir ];then
+		rm -rf $resDir
+	    fi
+	    mkdir $resDir
+	fi
+
+	#echo "do cosbench for issues $issue"
+	# cbTest/10m-delete.xml
+	idtSuffix=${issue#*/}		    #-->10m-delete.xml
+	idtSuffix=${idtSuffix%.*}	    #-->10m-delete
+	#echo $idtSuffix
+	startMon $idtSuffix
+	if [ -z $dryRun ];then
+	    #----cosbench
+	    :
+
+	fi
+	stopMonGetRet
+	sleep 1
+    done
+    postMon
+}
+
+function dorcb() {
+    cbTdir="./cbTest"
+
+    preMon
+    if [ X$optIssues != X ];then
+	issues=$optIssues
+    else
+	if [ X$testType == X ];then
+	    echo "testType NONE error,exit"
+	    exit
+	fi
+	issuesNew=""
+	for issue in $issues ;do
+	    issuesNew="$issuesNew cbTdir/$issue"
+	done
+	issues=$issuesNew
+    fi
+
+    echo "finally issues:
+	$issues
+    "
+    docbIssues "$issues"
+}
+
+dryRun=""
+cleanRun=""
+optIssues=""
+testType=""
+
+function usage () {
+    echo "Usage :  $0 [options] [optIssues]
+	Options:
+	-h	    Display this message
+	-d	    dryRun
+	-c	    doClean
+	-t	    testType
+    "
+}
+
+function main(){
+    while getopts "hdct:" opt;do
+    case $opt in
+        h)
+	    usage
+	    exit 0
+	    ;;
+	d)
+	    dryRun="True"
+	    ;;
+	c)
+	    cleanRun="True"
+	    ;;
+	t)
+	    testType="$OPTARG"
+    esac
+    done
+    shift $(($OPTIND-1))
+    optIssues=$@
+
+    doInit
+    if [ -n "$cleanRun" ];then
+	doClean
+    else
+	dorcb
+    fi
+}
+
+main $@
